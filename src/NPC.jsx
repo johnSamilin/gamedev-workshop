3f640776d92ae7cc020f9661821a2b1c2b7e0ebc
Alexander Saltykov <Alexander.Saltykov@dataart.com>
2021-04-15 14:54:35 +0300

    8. Анимация атаки и гибели

--- src/NPC.jsx
+++ src/NPC.jsx
@@ -1,26 +1,47 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { AnimationMixer, AnimationClip } from 'three';
 import { useFrame } from 'react-three-fiber';
 import { useGetModel } from './helpers/getModel';
 
 import run from './animations/run';
 import idle from './animations/idle';
+import die from './animations/die';
+import cast from './animations/cast';
 
-export const NPC = memo(({ model, id, isRunning, scale = [0.02, 0.02, 0.02] }) => {
+export const NPC = memo(({ model, id, isRunning, scale = [0.02, 0.02, 0.02], isAttacking, isDead }) => {
+  const [dead, setDead] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setDead(true), 5000);
+  }, []);
+
   const object = useGetModel(model, id);
   const mixer = new AnimationMixer(object);
   const runAnimation = mixer.clipAction(AnimationClip.parse(run));
   const idleAnimation = mixer.clipAction(AnimationClip.parse(idle));
+  const dieAnimation = mixer.clipAction(AnimationClip.parse(die));
+  const attackAnimation = mixer.clipAction(AnimationClip.parse(cast));
+
+  dieAnimation.clampWhenFinished = true;
+  dieAnimation.repetitions = 1;
+  attackAnimation.repetitions = 1;
 
   useEffect(() => {
-    if(isRunning) {
-      idleAnimation.stop();
-      runAnimation.play();
+    if (dead) {
+      mixer.stopAllAction();
+      dieAnimation.play();
     } else {
-      runAnimation.stop();
-      idleAnimation.play();
+      if(isRunning) {
+        idleAnimation.stop();
+        runAnimation.play();
+      } else {
+        runAnimation.stop();
+        idleAnimation.play();
+      }
+      if (isAttacking) {
+        attackAnimation.play();
+      }
     }
-  }, [isRunning, mixer]);
+  }, [isRunning, dead, mixer]);
 
   useFrame(() => {
     mixer.update(1 / 60);
