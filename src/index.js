b394dee778aa3189aa99d73f00ece616fd3d9547
Alexander Saltykov <Alexander.Saltykov@dataart.com>
2021-04-15 14:29:48 +0300

    1. Сцена, камера, коробка

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Canvas } from 'react-three-fiber';
 
 import './index.css';
 
 function Game() {
   return (
-    <>
-      
-    </>
+    <Canvas
+      camera={{
+        position: [20, 20, 20],
+        fov: 35,
+      }}
+    >
+      <axesHelper args={[2]} />
+      <mesh position={[0.5, 0.5, 0.5]}>
+        <boxBufferGeometry attach="geometry" />
+        <meshPhysicalMaterial attach="material" />
+      </mesh>
+    </Canvas>
   );
 }
 
