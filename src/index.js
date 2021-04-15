c452ca1900ee368db629e21fe0a4502fceac8ad9
Alexander Saltykov <Alexander.Saltykov@dataart.com>
2021-04-15 14:34:25 +0300

    2. Земля, сточники света, тени

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Canvas } from 'react-three-fiber';
+import { PCFSoftShadowMap } from 'three';
 
 import './index.css';
 
 function Game() {
   return (
     <Canvas
+      onCreated={({ gl }) => {
+        gl.shadowMap.enabled = true;
+        gl.shadowMap.type = PCFSoftShadowMap;
+      }}
       camera={{
         position: [20, 20, 20],
         fov: 35,
       }}
     >
       <axesHelper args={[2]} />
-      <mesh position={[0.5, 0.5, 0.5]}>
+      <mesh position={[0.5, 0.5, 0.5]} castShadow>
         <boxBufferGeometry attach="geometry" />
         <meshPhysicalMaterial attach="material" />
       </mesh>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+        <planeBufferGeometry attach="geometry" args={[100, 100, 10, 10]} />
+        <meshPhysicalMaterial attach="material" color="grey" />
+      </mesh>
+
+      <pointLight position={[2, 5, 2]} intensity={1} color="white" castShadow />
+      <pointLight position={[-2, 5, 2]} intensity={0.75} color={"blue"} castShadow />
     </Canvas>
   );
 }
